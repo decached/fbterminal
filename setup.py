c41263aa7ee5335a24527814951a76f9b597012a
Akash Kothawale <Ak4pict@gmail.com>
2013-01-18 17:42:31 +0530

    Update: Now stores .fbterminal at ~/ dir

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+import os
+import subprocess
 setup(
     name='fbterminal',
     version='0.1',
@@ -9,9 +10,8 @@ setup(
     author_email='io@decached.com',
     url='http://decached.com/fbterminal',
     install_requires='pycurl==7.19.0',
-    entry_points={
-        'console_scripts': ['fbterminal = fbterminal.terminal:command_line_runner',
-        ]
-    },
-    package_data={'fbterminal': ['.fbterminal']}
+    entry_points={'console_scripts': ['fbterminal = fbterminal.terminal:command_line_runner']},
+    data_files=[('/home/' + os.getlogin() + '/', ['fbterminal/.fbterminal'])]
 )
+
+os.system('chown ' + os.getlogin() + ' ~/.fbterminal')
