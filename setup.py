8291380d5dcb227090a250bc65939166376854bb
Akash Kothawale <akash@decached.com>
2013-05-25 17:56:00 +0530

    Lowered strictness of package requirement.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Akash Kothawale',
     author_email='io@decached.com',
     url='http://decached.com/fbterminal',
-    install_requires='pycurl==7.19.0',
+    install_requires='pycurl>=7.19.0',
     entry_points={'console_scripts': ['fbterminal = fbterminal.terminal:command_line_runner']},
     data_files=[('/home/' + os.getlogin() + '/', ['fbterminal/.fbterminal'])]
 )
